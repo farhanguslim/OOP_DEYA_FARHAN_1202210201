39f27e930158e2bbdb4cab68b410aa732eb14e1f
Muhammad Farhan Guslim <78484961+farhanguslim@users.noreply.github.com>
2022-11-04 16:53:25 +0700

    Update Server.java

--- Modul1/Server.java
+++ Modul1/Server.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Server {
 
-    public static void main(String[] args) {
-        // TODO Create Database
+ public static void main(String[] args) {
+        Database db = new Database();
 
-        // TODO Create Menu
+        Menu menu_1 = new Menu();
+        menu_1.setName("Bakso");
+        menu_1.setCategory("Makanan");
+        menu_1.setPrice(10000);
 
-        // TODO Insert Menu to Database
+        Menu menu_2 = new Menu();
+        menu_2.setName("Es Teh");
+        menu_2.setCategory("Minuman");
+        menu_2.setPrice(5000);
 
-
-        // TODO Display Main Menu
-
-        // TODO Create User
-
-        // Display Menu
+        Menu menu_3 = new Menu();
+        menu_3.setName("Brownies");
+        menu_3.setCategory("Dessert");
+        menu_3.setPrice(15000);
