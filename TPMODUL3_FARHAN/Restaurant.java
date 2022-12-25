0e8b8aaaad540b144e34636f702daffa7c32a748
Muhammad Farhan Guslim <78484961+farhanguslim@users.noreply.github.com>
2022-12-25 13:26:34 +0700

    Add files via upload

---
+++ TPMODUL3_FARHAN/Restaurant.java
@@ -0,0 +1,51 @@
+package TPMODUL3_FARHAN;
+
+public class Restaurant implements Runnable {
+    private boolean waitingForPickup = false;
+    private static final Object lock = new Object();
+    private static int FoodNumber = 1;
+
+    @Override
+    public void run() {
+        while (true) {
+            makeFood();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setWaitingForPickup(boolean waitingForPickup) {
+        this.waitingForPickup = waitingForPickup;
+    }
+
+    public static int getFoodNumber() {
+        return FoodNumber;
+    }
+
+    public void makeFood() {
+        synchronized (Restaurant.lock) {
+
+            if (this.waitingForPickup) {
+                try {
+                    System.out.println("Restaurant: Waiting for the Waiter to deliver the food");
+                    Restaurant.lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            waitingForPickup = true;
+            System.out.println("Restaurant:  Making Food Number " + FoodNumber++);
+            Restaurant.lock.notifyAll();
+            System.out.println("Restaurant: Telling the waiter to get the food\n");
+        }
+    }
+
+    public static Object getLock() {
+        return lock;
+    }
+}
